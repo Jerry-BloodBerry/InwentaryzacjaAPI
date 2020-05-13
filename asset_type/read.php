d517f412788edefe51c91af95a03ec6a21d24f25
Kamil M <k.mikolajczuk123@gmail.com>
2020-05-13 11:33:44 +0200

    Nieużywane

--- asset_type/read.php
+++
@@ -1,8 +0,0 @@
-<?php
-//required headers
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-include_once '../service/AssetTypeService.php';
-include_once '../service/RetrieverService.php';
-
-RetrieverService::RetrieveAllObjects(new AssetTypeService());
