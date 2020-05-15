aadd4892b2d0e6548d9e5dc7c69b0164abc55162
Sebastian Marciniak <sebastian.marciniak@gmail.com>
2020-05-15 17:20:29 +0200

    New Layout and bugfixing
    Fixed showing '?' as polish unicode characters by changing a default font.
    Add style for table.

--- generator/pdfGenerator.php
+++ generator/pdfGenerator.php
@@ -8,12 +8,44 @@ require '../vendor/autoload.php';
 use Spipu\Html2Pdf\Html2Pdf;
 
 $report = null;
-
+function AddStyle() :void
+{
+    $style =
+        "<style>
+            #container
+            {
+                text-align: center;
+                display: inline-block;
+            }
+            .position
+            {
+                text-align: left;            
+                margin-right: 10px;
+                margin-left: 15px;
+                font-size: 20px;             
+            }     
+        </style>";
+    echo $style;
+}
 function Title(string $name, int $nr, string $room, $date)
 {
     echo "<h4 style='text-align: right'>".$date->format('d-m-Y')."</h4>";
     echo "<h1 style='text-align: center'> Raport nr ".$nr."</h1>";
     echo "<h2 style='text-align: center'> Sala: ".$room."</h2>";
+    echo "<BR><BR><BR>";
+}
+function Content(Report $report) :void
+{
+    echo "<div id='container'>";
+    echo "<table>";
+    ShowTableHeader();
+    foreach ($report->getReportAssets() as $reportAsset)
+    {
+        ShowTableReportAssets($reportAsset);
+    }
+    echo "</table>";
+    echo "</div>";
+
 }
 function ReturnCurrentHTML()
 {
@@ -53,7 +85,7 @@ function FromWhereMoved(ReportAsset $asset) :string
 {
     if ($asset->getMoved())
     {
-        return $asset->getMovedFromRoom()->getName();
+        return $asset->getPreviousRoom()->getName();
     }
     else
         return "-";
@@ -62,16 +94,24 @@ function ShowTableReportAssets(ReportAsset $reportAsset) :void
 {
     echo "<tr>";
     echo "<td>";
+    echo "<div class='position'>";
     echo GetAssetName($reportAsset);
+    echo "</div>";
     echo "</td>";
     echo "<td>";
+    echo "<div class='position'>";
     echo GetAssetLetter_Id($reportAsset);
+    echo "</div>";
     echo "</td>";
     echo "<td>";
+    echo "<div class='position'>";
     echo IsMovedAsset($reportAsset);
+    echo "</div>";
     echo "</td>";
     echo "<td>";
+    echo "<div class='position'>";
     echo FromWhereMoved($reportAsset);
+    echo "</div>";
     echo "</td>";
     echo "</tr>";
 }
@@ -79,30 +119,27 @@ function ShowTableHeader() :void
 {
     echo "<tr>";
     echo "<td>";
+    echo "<div class='position'>";
     echo "Przedmiot";
+    echo "</div>";
     echo "</td>";
     echo "<td>";
+    echo "<div class='position'>";
     echo "ID";
+    echo "</div>";
     echo "</td>";
     echo "<td>";
+    echo "<div class='position'>";
     echo "Przeniesiony?";
+    echo "</div>";
     echo "</td>";
     echo "<td>";
+    echo "<div class='position'>";
     echo "Gdzie przeniesiony?";
+    echo "</div>";
     echo "</td>";
     echo "</tr>";
 }
-function Content(Report $report) :void
-{
-    echo "<table>";
-    ShowTableHeader();
-    foreach ($report->getReportAssets() as $reportAsset)
-    {
-        ShowTableReportAssets($reportAsset);
-    }
-    echo "</table>";
-}
-
 
 if (isset($_GET["id"]))
 {
@@ -122,12 +159,13 @@ else
     echo "No Id given!";
     exit();
 }
-
+AddStyle();
 Title($report->getReportHeader()->getName(), $report->getReportHeader()->getId(),
     $report->getReportHeader()->getRoom()->getName(), $report->getReportHeader()->getCreateDate());
 Content($report);
 
-$html2pdf = new Html2Pdf();
+$html2pdf = new Html2Pdf("P","A4","pl","UTF-8");
+$html2pdf->setDefaultFont("freesans");
 $html2pdf->writeHTML(ReturnCurrentHTML());
 $html2pdf->output();
 
--- object/ReportAsset.php
+++ object/ReportAsset.php
@@ -1,6 +1,4 @@
 <?php
-
-
 class ReportAsset implements JsonSerializable
 {
    private $asset;
